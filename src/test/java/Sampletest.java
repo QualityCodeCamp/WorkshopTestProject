847dc32c39487d0430c478a666bfa2ccf6d9de77
QualityCodeCamp <support@qualitycodecamp.com>
2023-04-03 16:02:18 -0400

    FirstChanges

---
+++ src/test/java/Sampletest.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Sampletest {
+
+    @Test
+    public void test1(){
+        Assert.assertTrue(true,"Test case passed");
+    }
+}
